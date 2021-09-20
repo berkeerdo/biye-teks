26c3567250f1604b94c4489e60f6335ee3dd8137
Berke Erdogan <mastic1998@hotmail.com>
2021-09-20 16:47:05 +0300

    Biye-Teks

--- src/components/navbar/dropdownToggle.jsx
+++ src/components/navbar/dropdownToggle.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const Dropdowntoggle = () => {
 
-    const [isShown, setIsShown] = React.useState(false)
-
   return (
-    <React.Fragment>
+    <>
 
-    </React.Fragment>
+    </>
   );
 };
 
